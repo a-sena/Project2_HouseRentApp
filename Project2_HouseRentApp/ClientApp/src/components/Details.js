b0cb8b07361748fc7ca32c94261708fd9eb56dbc
Sena Uysun <s364728@oslomet.no>
2023-11-11 21:26:41 +0100

    test

--- Project2_HouseRentApp/ClientApp/src/components/Details.js
+++ Project2_HouseRentApp/ClientApp/src/components/Details.js
@@ -4,31 +4,36 @@ import { useParams, useNavigate, Link } from "react-router-dom";
 import { Image, Button, Card, Grid, Icon } from 'semantic-ui-react';
 
 export function Details() {
-    const {id} = useParams()
+    const { id } = useParams()
     const navigate = useNavigate()
     const [details, setDetails] = useState({});
 
-     useEffect(() => {
-         axios.get("https://localhost:5001/Apartment/" + id)
-         .then(res => setDetails(res.data))
-         
-         .catch(err => console.log(err))
-    
-     }, [id]);
+    useEffect(() => {
+        axios.get("https://localhost:5001/Apartment/" + id)
+            .then(res => setDetails(res.data))
+
+            .catch(err => console.log(err))
+
+    }, [id]);
 
     return (
         <div className="container">
 
-        
+
             <div className='container p-5'>
-            <p>WELCOME USER THIS IS DETAILS PAGE</p>
-                <p>{details.id}</p>
-                <p>{details.price}</p>
-                <p>{details.description}</p>
+                <p>WELCOME USER THIS IS DETAILS PAGE</p>
+                <div>
+                    <Icon name="home" />
+                    <span>{details.square} m&sup2;</span>
+                </div>
+                <div>
+                    <Icon name="dollar sign" />
+                    <span>{details.price}</span>
+                </div>
                 <Link to={`https://localhost:5001/`}>
                     <Button
 
-                       
+
                         color="grey"
 
                         fluid > Back </Button></Link>
