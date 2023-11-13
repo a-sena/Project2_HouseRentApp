2257ee73e9ca8e150ecc0a4d93c654ca7fd56530
Nourhat <s351892@oslomet.no>
2023-11-13 21:54:39 +0100

    installing react-responsive-carousel package

--- Project2_HouseRentApp/ClientApp/src/App.js
+++ Project2_HouseRentApp/ClientApp/src/App.js
@@ -5,7 +5,6 @@ import { Layout } from './components/Layout';
 import './custom.css';
 import 'semantic-ui-css/semantic.min.css'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
->>>>>>> 8b234b5a28cab97d75abcd67f696018ceb8208e3
 export default class App extends Component {
   static displayName = App.name;
 
