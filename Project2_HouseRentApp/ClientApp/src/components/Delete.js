a9106fee11feb934f6df6242c8324fc65a779b49
Sena Uysun <s364728@oslomet.no>
2023-11-21 19:53:30 +0100

    delete page is edited

--- Project2_HouseRentApp/ClientApp/src/components/Delete.js
+++ Project2_HouseRentApp/ClientApp/src/components/Delete.js
@@ -9,41 +9,35 @@ import { Link } from "react-router-dom";
 
 export function Delete() {
 
- 
+ //useParams gets the ID from the website's route,
     const { id } = useParams();
     console.log(id);
    
   
-
+    //useState React Hooks is used to track the data value
+    //the initial value of data is specified as null
     const [data, setData] = useState({});
 
-   
+   //to navigate back to home page React useNavigation Hook is used
     const navigate = useNavigate();
 
-
+// We used useEffect to axious http get call. we are sending get request to url with spesific apartment id
+//In this way, information about a specific apartmant is obtained from the server 
     useEffect(() => {
         
-            var apartmentId = parseInt(id, 10);
+            var apartmentId = parseInt(id, 10); //Converting the id variable to integer
             axios.get("https://localhost:5001/Apartment/" + apartmentId)
-                .then(
-                    res => {
-                      
-                        setData(prevData => ({ ...prevData, ...res.data }));
-                      
-                       
-                        
-
-                    })
-
-                .catch(err => console.log(err));
-       
-    },[id]);
+            .then(res => {setData(prevData => ({ ...prevData, ...res.data })); //spread operation is used to object merging
+                })
+            .catch(err => console.log(err)); //if an error ocuurs, catch the error and write on the console.
+       },[id]);
 
    
    
-        console.log("Updated Data:", data);
+    console.log("This apartment is about to be deleted", data);
     
     const handleDelete = (id) => {
+        //Delete request with axious
         axios.delete(`https://localhost:5001/Delete/${id}`)
            
                
@@ -52,8 +46,8 @@ export function Delete() {
                // toast.error('Error deleting apartment');
             });
 
-        console.log(`House with ID ${id} deleted successfully.`);
-        navigate(`/`);
+        console.log("The apartment is deleted successfully.");
+        navigate("/");
 
     };
 
@@ -64,31 +58,42 @@ export function Delete() {
     
     return(
     <div className="container py-4">
-        <Link className="btn btn-primary" to="/">
-            back to Home
-        </Link>
+
         <h1 className="display-4">Apartment id: {id}</h1>
 
             <ul className="list-group w-50">
               
-                    <li className="list-group-item">Name: {data.name}</li>
-                    <li className="list-group-item">Address: {data.adress}</li>
-                    <li className="list-group-item">Price: {data.price}</li>
-                    <li className="list-group-item">Square: {data.square}</li>
-                    <li className="list-group-item">FirstRentalDate: {data.firtsRentalDate}</li>
-                    <li className="list-group-item">NumOfRooms: {data.numOfRooms}</li>
-            
+                <li className="list-group-item"><span style={{ fontWeight: 'bold' }}>Name:</span> {data.name}</li>
+                <li className="list-group-item"><span style={{ fontWeight: 'bold' }}>Address: </span>{data.adress}</li>
+                <li className="list-group-item"><span style={{ fontWeight: 'bold' }}>Price:</span> $ {data.price}</li>
+                <li className="list-group-item"><span style={{ fontWeight: 'bold' }}>Square:</span> {data.square} m&sup2;</li>
+                <li className="list-group-item"><span style={{ fontWeight: 'bold' }}>FirstRentalDate:</span> {data.firstRentalDate}</li>
+                <li className="list-group-item"><span style={{ fontWeight: 'bold' }}>NumOfRooms:</span> {data.numOfRooms}</li>
+                <li className="list-group-item"><span style={{ fontWeight: 'bold' }}>Description:</span> {data.description}</li>
+            </ul>
 
+            <div className="row" style={{ marginLeft: '60px', marginTop: '20px' }}>
+                <div className="col-2">
             <Button
 
                 name={data.id}
                 color="red"
                     onClick={() => handleDelete(data.id)}
                     fluid > Delete
-                </Button>
+                    </Button>
+                </div>
+                <div className="col-2">
 
-        </ul>
+                    <Link to="/">
+                        <Button
+                            name={data.id}
+                            color="grey"
 
+                            fluid> Back to Home</Button>
+                    
+                </Link>
+                </div>
+            </div>
         </div>
     )
 }
